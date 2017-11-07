5ed951d0fd25186389d108a2726dbf7a32fc6275
Paul Reiser <paul.reiser@stud.hs-hannover.de>
2017-11-07 20:24:49 +0100

    put () on tab/enter using topsy turvy

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -62,6 +62,8 @@
 
 #include "Kaleidoscope-LED-ActiveModColor.h"
 
+#include "Kaleidoscope-TopsyTurvy.h"
+
 // use Syster conjunction with Unicode
 // https://github.com/keyboardio/Kaleidoscope-Syster
 //#include "Kaleidoscope-Syster.h"
@@ -160,14 +162,14 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [DVORAK] = KEYMAP_STACKED
   (___,          Key_1,         Key_2,     Key_3,      Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Quote,     Key_Comma, Key_Period, Key_P, Key_Y, TD(LeftBrackets),
+   Key_Backtick, Key_Quote,     Key_Comma, Key_Period, Key_P, Key_Y, TOPSY(9),//TD(LeftBrackets),
    Key_Tab,   Key_A,         Key_O,     Key_E,      Key_U, Key_I,
    Key_PageDown, Key_Semicolon, Key_Q,     Key_J,      Key_K, Key_X, Key_Escape,
    OSM(LeftControl), Key_Space, OSM(LeftAlt), Key_Tab,
    ShiftToLayer(FN),
 
    M(MACRO_ANY),   Key_6, Key_7, Key_8, Key_9, Key_0, Key_KeypadNumLock,
-   TD(RightBrackets),      Key_F, Key_G, Key_C, Key_R, Key_L, Key_Slash,
+   TOPSY(0),/*TD(RightBrackets),*/      Key_F, Key_G, Key_C, Key_R, Key_L, Key_Slash,
                    Key_D, Key_H, Key_T, Key_N, Key_S, Key_Minus,
    Key_RightAlt,   Key_B, Key_M, Key_W, Key_V, Key_Z, Key_Equals,
    Key_RightGui, Key_Enter, Key_Backspace, OSM(RightShift),
@@ -440,7 +442,9 @@ void setup() {
 
     &TapDance,
 
-    &ActiveModColorEffect//,
+    &ActiveModColorEffect,
+
+    &TopsyTurvy//,
 
     //    &Unicode,
 
